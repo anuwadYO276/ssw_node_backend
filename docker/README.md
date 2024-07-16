5c8845f96f5292d23b68dec73ef74fda26141c77
anuwadYO276 <158416918+anuwadYO276@users.noreply.github.com>
2024-07-16 11:07:27 +0700

    up

---
+++ docker/README.md
@@ -0,0 +1,5 @@
+>npm install
+
+== produciton ==
+
+>> docker compose -f "docker-compose-prod.yml" up -d --build
--- sequelize.js
+++ sequelize.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('db_ssw_dlt', 'swp_root', 'Password123#@!', {
-    host: '10.1.112.83',
-    dialect: 'mysql'
-  });
+  host: '10.1.112.83',
+  dialect: 'mysql'
+});
+
 module.exports = sequelize;
